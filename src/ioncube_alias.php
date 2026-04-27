f32f909d3af5dbca6661849f65de177d565685af
riezaekatomara <riezaekatomara@gmail.com>
2026-04-27 11:23:02 +0700

    fix: add ioncube function alias to bypass missing _il_exec

---
+++ src/ioncube_alias.php
@@ -0,0 +1,7 @@
+<?php
+if (!function_exists('_il_exec') && function_exists('_dyuweyrj4')) {
+    function _il_exec() {
+        return call_user_func_array('_dyuweyrj4', func_get_args());
+    }
+}
+?>
