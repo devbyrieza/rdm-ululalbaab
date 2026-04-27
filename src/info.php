c7391450839aecde77f37fa98588b80f1b97744c
riezaekatomara <riezaekatomara@gmail.com>
2026-04-27 10:51:58 +0700

    debug: check extension status

--- src/info.php
+++ src/info.php
@@ -1,3 +1,11 @@
 <?php
+echo "<h1>Ioncube Check</h1>";
+if (extension_loaded('ionCube Loader')) {
+    echo "<p style='color:green'>Extension 'ionCube Loader' is LOADED</p>";
+} else {
+    echo "<p style='color:red'>Extension 'ionCube Loader' is NOT LOADED</p>";
+}
+
+echo "<h2>PHP Info</h2>";
 phpinfo();
 ?>
