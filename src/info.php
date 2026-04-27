8cde2a9c25a84da282c723a0ec39e23efda8d68b
riezaekatomara <riezaekatomara@gmail.com>
2026-04-27 10:54:59 +0700

    debug: check _il_exec function

--- src/info.php
+++ src/info.php
@@ -1,5 +1,11 @@
 <?php
-echo "<h1>Ioncube Check</h1>";
+echo "<h1>Ioncube Function Check</h1>";
+if (function_exists('_il_exec')) {
+    echo "<p style='color:green'>Function '_il_exec' EXISTS</p>";
+} else {
+    echo "<p style='color:red'>Function '_il_exec' DOES NOT EXIST</p>";
+}
+
 if (extension_loaded('ionCube Loader')) {
     echo "<p style='color:green'>Extension 'ionCube Loader' is LOADED</p>";
 } else {
