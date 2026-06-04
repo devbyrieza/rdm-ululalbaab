a6f6ddbb3ccf803c2a05ca1f2a8cfbd4d70619d7
riezaekatomara <riezaekatomara@gmail.com>
2026-06-04 18:51:17 +0700

    revert: fix: force https to resolve mixed content

--- src/ioncube_alias.php
+++ src/ioncube_alias.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Ioncube, Database & HTTPS Bridge
+ * Ioncube & Database Bridge + Debugger
  */
 
 // 1. Alias fungsi Ioncube
@@ -10,24 +10,27 @@ if (!function_exists('_il_exec') && function_exists('_dyuweyrj4')) {
     }
 }
 
-// 2. Paksa HTTPS (Fix Mixed Content / Stuck 100% Loading)
-$_SERVER['HTTPS'] = 'on';
-$_SERVER['SERVER_PORT'] = 443;
+// 2. Logging untuk Debug
+$log_file = dirname(__FILE__) . '/debug_rdm.log';
+function rdm_log($msg) {
+    global $log_file;
+    file_put_contents($log_file, "[" . date('Y-m-d H:i:s') . "] " . $msg . "\n", FILE_APPEND);
+}
+
+rdm_log("Request started: " . $_SERVER['REQUEST_URI']);
 
 // 3. Suntikan Database
-global $db, $config;
-if (!isset($db)) { $db = array(); }
-if (!isset($config)) { $config = array(); }
+global $db;
+if (!isset($db)) {
+    $db = array();
+}
 
-// Paksa base_url agar pakai HTTPS
-$config['base_url'] = 'https://rdm.alandalus-ululalbaab.com/';
-
-$db['default'] = array(
+$db_config = array(
     'dsn'      => '',
     'hostname' => 'pc80okgks0ocw8kcowogckkk',
     'username' => 'root',
     'password' => 'j6ldBKvb8L7xiydSLkO8mr6ng8eIyn7zXqLMGUBqhz9Ynec2IiVNXmharN0JN7wU',
-    'database' => 'rdm_ululalbaab',
+    'database' => 'rdm_ululalbaab', // Coba ganti ke rdm_ululalbaab
     'dbdriver' => 'mysqli',
     'dbprefix' => '',
     'pconnect' => FALSE,
@@ -44,5 +47,11 @@ $db['default'] = array(
     'save_queries' => TRUE
 );
 
-// Hapus logging agar kencang
-// rdm_log("..."); 
+$db['default'] = $db_config;
+rdm_log("Database config injected into global \$db");
+
+// 4. Deteksi jika tertimpa
+register_shutdown_function(function() {
+    global $db;
+    rdm_log("Request finished. Final DB Database: " . ($db['default']['database'] ?? 'NOT SET'));
+});
